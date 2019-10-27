bb61475d818e7abb9e7b27ab55c0882d4000a542
Chris Carr <chris@appthem.com>
2019-10-27 18:38:10 +1100

    Created lestest from template

---
+++ ledtest/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimpleLed.Services;
+
+namespace SimpleLed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create service collection and configure our services
+            var services = ConfigureServices();
+            
+            // Generate a provider
+            var serviceProvider = services.BuildServiceProvider();
+   
+            // Kick off our actual code
+            serviceProvider.GetService<Startup>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddTransient<ITestService, TestService>();
+            
+            // IMPORTANT! Register our application entry point
+            services.AddTransient<Startup>();
+
+            return services;
+        }
+    }
+}
